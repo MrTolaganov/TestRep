b52738ae1f428d81b8686dbfa51e4f1af5130cae
Otabek <otabektolaganov69@gmail.com>
2023-11-20 09:59:39 +0500

    20/11/2023 10:00

--- homework/hw1.js
+++ homework/hw1.js
@@ -2,8 +2,8 @@ const age = +prompt("How old are you?", "");
 
 if (age > 25) {
     console.log("25+");
+} else if (age > 18) {
+    console.log("18+");
 } else {
     console.log("18-");
 }
-
-(age > 25) ? console.log("25+") : console.log("25-");
---
+++ homework/tempCodeRunnerFile.js
@@ -0,0 +1,10 @@
+const thief = {
+    jacket: "Black",
+}
+
+const property = "age";
+const value = "30";
+
+thief[property] = value;
+
+console.log(thief);
