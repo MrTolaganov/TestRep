325aa7d28c65fc3fa040060c001d5919f86dd8a6
Otabek <otabektolaganov69@gmail.com>
2023-11-21 23:39:14 +0500

    21/11/2023 23:40

--- script.js
+++ script.js
@@ -1,10 +1,37 @@
-const thief = {
-  jacket: "black",
-  height: 1.7,
-  colors: {
-    hair: "grey",
-    style: "curley",
-  },
-};
+const arr = [6, 5, 4, 3, 2, 1];
 
-console.log(Object.keys(thief));
+// arr.pop();
+// arr.push(7);
+
+// arr.shift();
+// arr.unshift(0);
+
+// console.log(arr);
+
+// for (let i = 0; i < arr.length; i++) {
+//   console.log(arr[i]);
+// }
+
+// for (let i in arr) {
+//   console.log(i);
+// }
+
+// for (let i of arr) {
+//   console.log(i);
+// }
+
+// arr[99] = 0;
+// console.log(arr.length);
+// console.log(arr);
+
+// arr.forEach(function (item, index, arr) {
+//   console.log(`${index}: ${item} into arr ${arr}`);
+// });
+
+// const movies = prompt("What's your favourite movies", "");
+// const userMovies = movies.split(", ");
+// userMovies.sort();
+// console.log(userMovies.join("- "));
+
+arr.sort();
+console.log(arr);
