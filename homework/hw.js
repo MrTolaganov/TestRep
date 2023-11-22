750098b64c75ea56e2e662f3fcb3676f2f386778
Otabek <otabektolaganov69@gmail.com>
2023-11-22 19:09:14 +0500

    22/11/2023 19:09

--- homework/hw.js
+++ homework/hw.js
@@ -1,73 +1,74 @@
-let numberOfSeries;
-
-function startApp() {
-  numberOfSeries = +prompt("How many series did you watch?", "");
-
-  while (
-    numberOfSeries == "" ||
-    numberOfSeries == null ||
-    isNaN(numberOfSeries)
-  ) {
-    numberOfSeries = +prompt("How many series did you watch?", "");
-  }
-}
-
-startApp();
-
 const seriesDB = {
-  coumt: numberOfSeries,
+  count: 0,
   series: {},
   actors: {},
   genres: [],
   private: false,
-};
-
-const rememberMySeries = function () {
-  for (let i = 0; i < 2; i++) {
-    const a = prompt("Which latest series did you watch?");
-    const b = prompt("How do you estimate?");
-
-    if (a != "" && b != "" && a != null && b != null) {
-      seriesDB.series[a] = b;
-      console.log("done");
-    } else {
-      console.log("error");
-      i--;
+  start: function () {
+    seriesDB.count = +prompt("How many series did you watch?", "");
+    while (
+        seriesDB.count === "" ||
+        seriesDB.count == null ||
+        isNaN(seriesDB.count)
+        ) {
+      seriesDB.count = +prompt("How many series did you watch?", "");
     }
-  }
+  },
+  rememberMySeries: function () {
+    for (let i = 0; i < 2; i++) {
+      const a = prompt("Which latest series did you watch?");
+      const b = prompt("How do you estimate?");
+      if (a !== "" && b !== "" && a != null && b != null) {
+        seriesDB.series[a] = b;
+        console.log("done");
+      } else {
+        console.log("error");
+        i--;
+      }
+    }
+  },
+  detectLevelSeries: function () {
+    if (seriesDB.count < 5) {
+      console.log("Kam serial ko'ribsiz");
+    } else if (seriesDB.count > 5 && seriesDB.count < 10) {
+      console.log("Siz klassik tomoshabin ekansiz");
+    } else {
+      console.log("Siz serialchi zvezda ekansiz");
+    }
+  },
+  showDB: function () {
+    if (seriesDB.private) {
+      console.log(seriesDB);
+    } else {
+      console.log("");
+    }
+  },
+  writeGenres: function () {
+    // for (let i = 0; i < 3; i++) {
+    //   let genre = prompt(`Yaxshi ko'rgan janringiz ${i + 1}`, "");
+    //   if (genre === "" || genre === null) {
+    //     console.log("Siz noto'g'ri ma'lumot kiritdingiz");
+    //     i--;
+    //   } else {
+    //     seriesDB.genres[i] = genre;
+    //   }
+    // }
+    let genres = prompt("Yaxshi ko'rgan janringizni vergul yordamida kiriting", "").toLowerCase();
+    if (genres === "" || genres === null) {
+      console.log("Siz noto'g'ri ma'lumot kiritdingiz");
+    } else {
+      seriesDB.genres = genres.split(", ");
+      seriesDB.genres.sort();
+    }
+    seriesDB.genres.forEach((item, index) => {
+      console.log(`Siz yaxshi ko'rgan janringiz ${index + 1} - nomi ${item}`);
+    })
+  },
+  visibleDB: function () {
+    if (seriesDB.private) {
+      seriesDB.private = false;
+    } else {
+      seriesDB.private = true;
+    }
+  },
 };
-
-// rememberMySeries();
-
-const detectLevelSeries = () => {
-  if (numberOfSeries < 5) {
-    console.log("Kam serial ko'ribsiz");
-  } else if (numberOfSeries > 5 && numberOfSeries < 10) {
-    console.log("Siz klassik tomoshabin ekansiz");
-  } else {
-    console.log("Siz serialchi zvezda ekansiz");
-  }
-};
-
-// detectLevelSeries();
-
-const showDB = (hidden) => {
-  if (! hidden) {
-    console.log(seriesDB);
-  } else {
-    console.log("");
-  }
-}
-
-showDB(seriesDB.private);
-
-const writeGenres = () => {
-  for (let i = 0; i < 3; i++) {
-    let genre = prompt("Yaxshi ko'rgan janringiz", "");
-    console.log(genre);
-    seriesDB.genres[i] = genre;
-  }
-}
-
-// writeGenres();
-// console.log(seriesDB.genres);
